5a68308ac244d6ca457529e7679631148d631dc4
Harman3011 <133263990+Harman3011@users.noreply.github.com>
2023-07-09 16:19:57 -0400

    ICE7

---
+++ MDEV1001-M2023-ICE7/MDEV1001-M2023-ICE7/MovieTableViewCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+class MovieTableViewCell: UITableViewCell {
+@IBOutlet weak var titleLabel: UITextField!
+@IBOutlet weak var studioLabel: UITextField!
+@IBOutlet weak var ratingLabel: UITextField!
+}
